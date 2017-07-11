3deac27487c8f7832322b823921ac6988e123e52
Cinder Biscuits <cinder@cinderblocks.biz>
2017-07-10 21:29:48 -0500

    Oops, forgot to commit MultiValueDict.cs

---
+++ LibreMetaverse/MultiValueDict.cs
@@ -0,0 +1,152 @@
+﻿/*
+ * Copyright(c) 2008 Solutions Design.
+ * Copyright(c) 2017 openmetaverse.co. 
+ * All rights reserved.
+ *
+ * - Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * - Redistributions of source code must retain the above copyright notice, this
+ 
+ * list of conditions and the following disclaimer.
+ * - Neither the name of the openmetaverse.co nor the names
+ *   of its contributors may be used to endorse or promote products derived from
+ *   this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT(INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LibreMetaverse
+{
+    /// <summary>
+    /// Extension to the normal Dictionary. This class can store more than one value for every key. It keeps a HashSet for every Key value.
+    /// Calling Add with the same Key and multiple values will store each value under the same Key in the Dictionary. Obtaining the values
+    /// for a Key will return the HashSet with the Values of the Key. 
+    /// </summary>
+    /// <typeparam name="TKey">The type of the key.</typeparam>
+    /// <typeparam name="TValue">The type of the value.</typeparam>
+    public class MultiValueDictionary<TKey, TValue> : Dictionary<TKey, List<TValue>>
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MultiValueDictionary&lt;TKey, TValue&gt;"/> class.
+        /// </summary>
+        public MultiValueDictionary() : base()
+        {
+        }
+
+
+        /// <summary>
+        /// Adds the specified value under the specified key
+        /// </summary>
+        /// <param name="key">The key.</param>
+        /// <param name="value">The value.</param>
+        public void Add(TKey key, TValue value)
+        {
+            if (key == null) throw new ArgumentNullException(nameof(key));
+            List<TValue> container = null;
+            if (!this.TryGetValue(key, out container))
+            {
+                container = new List<TValue>();
+                base.Add(key, container);
+            }
+            container.Add(value);
+        }
+
+
+        /// <summary>
+        /// Determines whether this dictionary contains the specified value for the specified key 
+        /// </summary>
+        /// <param name="key">The key.</param>
+        /// <param name="value">The value.</param>
+        /// <returns>true if the value is stored for the specified key in this dictionary, false otherwise</returns>
+        public bool ContainsValue(TKey key, TValue value)
+        {
+            if (key == null) throw new ArgumentNullException(nameof(key));
+            bool toReturn = false;
+            List<TValue> values = null;
+            if (this.TryGetValue(key, out values))
+            {
+                toReturn = values.Contains(value);
+            }
+            return toReturn;
+        }
+
+
+        /// <summary>
+        /// Removes the specified value for the specified key. It will leave the key in the dictionary.
+        /// </summary>
+        /// <param name="key">The key.</param>
+        /// <param name="value">The value.</param>
+        public void Remove(TKey key, TValue value)
+        {
+            if (key == null) throw new ArgumentNullException(nameof(key));
+
+            List<TValue> container = null;
+            if (this.TryGetValue(key, out container))
+            {
+                container.Remove(value);
+                if (container.Count <= 0)
+                {
+                    this.Remove(key);
+                }
+            }
+        }
+
+
+        /// <summary>
+        /// Merges the specified multivaluedictionary into this instance.
+        /// </summary>
+        /// <param name="toMergeWith">To merge with.</param>
+        public void Merge(MultiValueDictionary<TKey, TValue> toMergeWith)
+        {
+            if (toMergeWith == null)
+            {
+                return;
+            }
+
+            foreach (KeyValuePair<TKey, List<TValue>> pair in toMergeWith)
+            {
+                foreach (TValue value in pair.Value)
+                {
+                    this.Add(pair.Key, value);
+                }
+            }
+        }
+
+
+        /// <summary>
+        /// Gets the values for the key specified. This method is useful if you want to avoid an exception for key value retrieval and you can't use TryGetValue
+        /// (e.g. in lambdas)
+        /// </summary>
+        /// <param name="key">The key.</param>
+        /// <param name="returnEmptySet">if set to true and the key isn't found, an empty hashset is returned, otherwise, if the key isn't found, null is returned</param>
+        /// <returns>
+        /// This method will return null (or an empty set if returnEmptySet is true) if the key wasn't found, or
+        /// the values if key was found.
+        /// </returns>
+        public List<TValue> GetValues(TKey key, bool returnEmptySet)
+        {
+            List<TValue> toReturn = null;
+            if (!base.TryGetValue(key, out toReturn) && returnEmptySet)
+            {
+                toReturn = new List<TValue>();
+            }
+            return toReturn;
+        }
+    }
+}
