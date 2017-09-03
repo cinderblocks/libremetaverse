59f4d5980431f60c90eb26ff5d1a6e6165c6c655
Cinder Biscuits <cinder@cinderblocks.biz>
2017-09-03 18:25:24 -0500

    Add PrimMesher to build projects, remove blob

---
+++ PrimMesher/ObjMesh.cs
@@ -0,0 +1,220 @@
+﻿/*
+ * Copyright (c) Contributors
+ * See CONTRIBUTORS.TXT for a full list of copyright holders.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the OpenSimulator Project nor the
+ *       names of its contributors may be used to endorse or promote products
+ *       derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE DEVELOPERS ``AS IS'' AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE CONTRIBUTORS BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace PrimMesher
+{
+    public class ObjMesh
+    {
+        List<Coord> coords = new List<Coord>();
+        List<Coord> normals = new List<Coord>();
+        List<UVCoord> uvs = new List<UVCoord>();
+
+        public string meshName = string.Empty;
+        public List<List<ViewerVertex>> viewerVertices = new List<List<ViewerVertex>>();
+        public List<List<ViewerPolygon>> viewerPolygons = new List<List<ViewerPolygon>>();
+
+        List<ViewerVertex> faceVertices = new List<ViewerVertex>();
+        List<ViewerPolygon> facePolygons = new List<ViewerPolygon>();
+        public int numPrimFaces;
+
+        Dictionary<int, int> viewerVertexLookup = new Dictionary<int, int>();
+
+        public ObjMesh(string path)
+        {
+            ProcessStream(new StreamReader(path));
+        }
+
+
+        public ObjMesh(StreamReader sr)
+        {
+            ProcessStream(sr);
+        }
+
+
+        private void ProcessStream(StreamReader s)
+        {
+            numPrimFaces = 0;
+
+            while (!s.EndOfStream)
+            {
+                string line = s.ReadLine().Trim();
+                string[] tokens = Regex.Split(line, @"\s+");
+
+                // Skip blank lines and comments
+                if (tokens.Length > 0 && tokens[0] != String.Empty && !tokens[0].StartsWith("#"))
+                    ProcessTokens(tokens);
+            }
+            MakePrimFace();
+        }
+
+        public VertexIndexer GetVertexIndexer()
+        {
+            VertexIndexer vi = new VertexIndexer();
+            vi.numPrimFaces = this.numPrimFaces;
+            vi.viewerPolygons = this.viewerPolygons;
+            vi.viewerVertices = this.viewerVertices;
+
+            return vi;
+        }
+
+
+        private void ProcessTokens(string[] tokens)
+        {
+            switch (tokens[0].ToLower())
+            {
+                case "o":
+                    meshName = tokens[1];
+                    break;
+
+                case "v":
+                    coords.Add(ParseCoord(tokens));
+                    break;
+
+                case "vt":
+                    {
+                        uvs.Add(ParseUVCoord(tokens));
+                        break;
+                    }
+
+                case "vn":
+                    normals.Add(ParseCoord(tokens));
+                    break;
+
+                case "g":
+
+                    MakePrimFace();
+
+                    break;
+
+                case "s":
+                    break;
+
+                case "f":
+
+                    int[] vertIndices = new int[3];
+
+                    for (int vertexIndex = 1; vertexIndex <= 3; vertexIndex++)
+                    {
+                        string[] indices = tokens[vertexIndex].Split('/');
+
+                        int positionIndex = int.Parse(indices[0],
+                            CultureInfo.InvariantCulture) - 1;
+
+                        int texCoordIndex = -1;
+                        int normalIndex = -1;
+
+                        if (indices.Length > 1)
+                        {
+
+                            if (int.TryParse(indices[1], System.Globalization.NumberStyles.Integer, CultureInfo.InvariantCulture, out texCoordIndex))
+                                texCoordIndex--;
+                            else texCoordIndex = -1;
+
+                        }
+
+                        if (indices.Length > 2)
+                        {
+                            if (int.TryParse(indices[1], System.Globalization.NumberStyles.Integer, CultureInfo.InvariantCulture, out normalIndex))
+                                normalIndex--;
+                            else normalIndex = -1;
+                        }
+
+                        int hash = hashInts(positionIndex, texCoordIndex, normalIndex);
+
+                        if (viewerVertexLookup.ContainsKey(hash))
+                            vertIndices[vertexIndex - 1] = viewerVertexLookup[hash];
+                        else
+                        {
+                            ViewerVertex vv = new ViewerVertex();
+                            vv.v = coords[positionIndex];
+                            if (normalIndex > -1)
+                                vv.n = normals[normalIndex];
+                            if (texCoordIndex > -1)
+                                vv.uv = uvs[texCoordIndex];
+                            faceVertices.Add(vv);
+                            vertIndices[vertexIndex - 1] = viewerVertexLookup[hash] = faceVertices.Count - 1;
+                        }
+                    }
+
+                    facePolygons.Add(new ViewerPolygon(vertIndices[0], vertIndices[1], vertIndices[2]));
+                    break;
+
+                case "mtllib":
+                    break;
+
+                case "usemtl":
+                    break;
+
+                default:
+                    break;
+            }
+        }
+
+
+        private void MakePrimFace()
+        {
+            if (faceVertices.Count > 0 && facePolygons.Count > 0)
+            {
+                viewerVertices.Add(faceVertices);
+                faceVertices = new List<ViewerVertex>();
+                viewerPolygons.Add(facePolygons);
+
+                facePolygons = new List<ViewerPolygon>();
+
+                viewerVertexLookup = new Dictionary<int, int>();
+
+                numPrimFaces++;
+            }
+        }
+
+        private UVCoord ParseUVCoord(string[] tokens)
+        {
+            return new UVCoord(
+                float.Parse(tokens[1], CultureInfo.InvariantCulture),
+                float.Parse(tokens[1], CultureInfo.InvariantCulture));
+        }
+
+        private Coord ParseCoord(string[] tokens)
+        {
+            return new Coord(
+                float.Parse(tokens[1], CultureInfo.InvariantCulture),
+                float.Parse(tokens[2], CultureInfo.InvariantCulture),
+                float.Parse(tokens[3], CultureInfo.InvariantCulture));
+        }
+
+        private int hashInts(int i1, int i2, int i3)
+        {
+            return (i1.ToString() + " " + i2.ToString() + " " + i3.ToString()).GetHashCode();
+        }
+    }
+}
---
+++ PrimMesher/VertexIndexer.cs
@@ -0,0 +1,152 @@
+﻿/*
+ * Copyright (c) Contributors
+ * See CONTRIBUTORS.TXT for a full list of copyright holders.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the OpenSimulator Project nor the
+ *       names of its contributors may be used to endorse or promote products
+ *       derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE DEVELOPERS ``AS IS'' AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE CONTRIBUTORS BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PrimMesher
+{
+    public struct ViewerVertex
+    {
+        public Coord v;
+        public Coord n;
+        public UVCoord uv;
+
+        public ViewerVertex(Coord coord, Coord normal, UVCoord uv)
+        {
+            this.v = coord;
+            this.n = normal;
+            this.uv = uv;
+        }
+    }
+
+    public struct ViewerPolygon
+    {
+        public int v1;
+        public int v2;
+        public int v3;
+
+        public ViewerPolygon(int v1, int v2, int v3)
+        {
+            this.v1 = v1;
+            this.v2 = v2;
+            this.v3 = v3;
+        }
+    }
+
+    public class VertexIndexer
+    {
+        public List<List<ViewerVertex>> viewerVertices;
+        public List<List<ViewerPolygon>> viewerPolygons;
+        public int numPrimFaces;
+        private int[][] viewerVertIndices;
+
+        public VertexIndexer()
+        {
+        }
+
+
+        public VertexIndexer(PrimMesh primMesh)
+        {
+            int maxPrimFaceNumber = 0;
+
+            foreach (ViewerFace vf in primMesh.viewerFaces)
+                if (maxPrimFaceNumber < vf.primFaceNumber)
+                    maxPrimFaceNumber = vf.primFaceNumber;
+
+            this.numPrimFaces = maxPrimFaceNumber + 1;
+
+            int[] numViewerVerts = new int[numPrimFaces];
+            int[] numVertsPerPrimFace = new int[numPrimFaces];
+
+            for (int i = 0; i < numPrimFaces; i++)
+            {
+                numViewerVerts[i] = 0;
+                numVertsPerPrimFace[i] = 0;
+            }
+
+            foreach (ViewerFace vf in primMesh.viewerFaces)
+                numVertsPerPrimFace[vf.primFaceNumber] += 3;
+
+            this.viewerVertices = new List<List<ViewerVertex>>(numPrimFaces);
+            this.viewerPolygons = new List<List<ViewerPolygon>>(numPrimFaces);
+            this.viewerVertIndices = new int[numPrimFaces][];
+
+            // create index lists
+            for (int primFaceNumber = 0; primFaceNumber < numPrimFaces; primFaceNumber++)
+            {
+                //set all indices to -1 to indicate an invalid index
+                int[] vertIndices = new int[primMesh.coords.Count];
+                for (int i = 0; i < primMesh.coords.Count; i++)
+                    vertIndices[i] = -1;
+                viewerVertIndices[primFaceNumber] = vertIndices;
+                
+                viewerVertices.Add(new List<ViewerVertex>(numVertsPerPrimFace[primFaceNumber]));
+                viewerPolygons.Add(new List<ViewerPolygon>());
+            }
+
+            // populate the index lists
+            foreach (ViewerFace vf in primMesh.viewerFaces)
+            {
+                int v1, v2, v3;
+                
+                int[] vertIndices = viewerVertIndices[vf.primFaceNumber];
+                List<ViewerVertex> viewerVerts = viewerVertices[vf.primFaceNumber];
+
+                // add the vertices
+                if (vertIndices[vf.coordIndex1] < 0)
+                {
+                    viewerVerts.Add(new ViewerVertex(vf.v1, vf.n1, vf.uv1));
+                    v1 = viewerVerts.Count - 1;
+                    vertIndices[vf.coordIndex1] = v1;
+                }
+                else v1 = vertIndices[vf.coordIndex1];
+
+                if (vertIndices[vf.coordIndex2] < 0)
+                {
+                    viewerVerts.Add(new ViewerVertex(vf.v2, vf.n2, vf.uv2));
+                    v2 = viewerVerts.Count - 1;
+                    vertIndices[vf.coordIndex2] = v2;
+                }
+                else v2 = vertIndices[vf.coordIndex2];
+
+                if (vertIndices[vf.coordIndex3] < 0)
+                {
+                    viewerVerts.Add(new ViewerVertex(vf.v3, vf.n3, vf.uv3));
+                    v3 = viewerVerts.Count - 1;
+                    vertIndices[vf.coordIndex3] = v3;
+                }
+                else v3 = vertIndices[vf.coordIndex3];
+
+                viewerPolygons[vf.primFaceNumber].Add(new ViewerPolygon(v1, v2, v3));
+            }
+
+        }
+    }
+}
