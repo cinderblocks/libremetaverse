24df5f162c9bf96bd7e7a5345ac5e717eaff08d0
Cinder <cinder@cinderblocks.biz>
2021-07-24 09:19:52 -0500

    More on codesigning

---
+++ util/SignPackages.ps1
@@ -0,0 +1,29 @@
+<#
+ # Copyright (c) 2021, Sjofn LLC. All rights reserved.
+ #
+ # Permission to use, copy, modify, and/or distribute this script for any
+ # purpose without fee is hereby granted.
+ # 
+ # THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES 
+ # WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ # MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR 
+ # ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES 
+ # WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN 
+ # ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF 
+ # OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE. 
+ #>
+
+ param([String]$PfxPasswd)
+ 
+ Write-Output "Signing nupkgs..."
+
+Get-ChildItem -Filter "*.nupkg" -Path "C:\Users\appveyor\AppData\Local\Temp\" -recurse | ForEach {
+	Write-Output $("Signing " + $_.Name + "...")
+	nuget sign $_.FullName -NonInteractive -Verbosity quiet `
+						   -CertificateFingerprint 95AEDF70A4C4E6184F43CA8C1E17B6D838430184 `
+						   -CertificateStoreLocation "LocalMachine" `
+						   -CertificatePassword $PfxPasswd `
+						   -Timestamper http://timestamp.comodoca.com
+ }
+
+ Write-Output "Signing complete."
