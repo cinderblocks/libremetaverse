d07c20ee567098e9db373f6f339f591d13f23450
Cinder <cinder@cinderblocks.biz>
2022-01-05 09:14:34 -0600

    Case sensitive filesystems................

--- mapgen.bat
+++ mapgen.bat
@@ -1,1 +1,1 @@
-"bin/mapgenerator.exe" data/message_template.msg Programs/mapgenerator/template.cs Programs/mapgenerator/unusedpackets.txt LibreMetaverse/_Packets_.cs
+"bin/mapgenerator.exe" data/message_template.msg Programs/mapgenerator/template.cs Programs/mapGenerator/unusedpackets.txt LibreMetaverse/_Packets_.cs
