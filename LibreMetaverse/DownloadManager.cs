94135859f0015f517eb03b2d7e9bad5d624fad5d
Cinder <cinder@cinderblocks.biz>
2022-01-23 13:31:56 -0600

    Set MaxIdleTime for http downloads to 60 seconds as per its comment (which is copied and pasted from CapsBase) this may need some tweaking to find the optimum setting.

--- LibreMetaverse/DownloadManager.cs
+++ LibreMetaverse/DownloadManager.cs
@@ -130,7 +130,7 @@ namespace OpenMetaverse
                 request.ClientCertificates.Add(ClientCert);
 
             // Leave idle connections to this endpoint open for up to 60 seconds
-            request.ServicePoint.MaxIdleTime = 0;
+            request.ServicePoint.MaxIdleTime = 1000 * 60;
             // Disable stupid Expect-100: Continue header
             request.ServicePoint.Expect100Continue = false;
             // Crank up the max number of connections per endpoint
