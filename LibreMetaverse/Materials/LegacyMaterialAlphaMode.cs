15e74f82daaa60b27ffebb229015bff784dae586
Adam Frisby <adam@sinewavecompany.com>
2024-08-17 00:28:36 +1000

    * Adds missing LegacyMaterial class and LegacyMaterialAlphaMode enum.

---
+++ LibreMetaverse/Materials/LegacyMaterialAlphaMode.cs
@@ -0,0 +1,11 @@
+﻿namespace LibreMetaverse.Materials
+{
+    public enum LegacyMaterialAlphaMode : byte
+    {
+        None = 0,
+        Blend = 1,
+        Mask = 2,
+        Emissive = 3,
+        Default = 4,
+    }
+}
